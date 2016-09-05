13f78f05a3fd2ac91146c86bbcbf8fcc1c24e727
NthPortal <nthportal@gmail.com>
2016-09-05 04:28:00 -0400

    Add size limits for paste metadata

--- app/io/github/nthportal/paste/core/conf/Conf.scala
+++ app/io/github/nthportal/paste/core/conf/Conf.scala
@@ -1,3 +1,11 @@
 package io.github.nthportal.paste.core.conf
 
 case class Conf(maxPasteSizeKB: Long = 2000, maxTotalDiskUsageKB: Long = 5000000L)
+
+object Conf {
+  object Static {
+    val maxTitleLengthChars = 64
+    val maxAuthorLengthChars = 64
+    val maxDescriptionLengthChars = 200
+  }
+}
--- app/models/PasteData.scala
+++ app/models/PasteData.scala
@@ -1,15 +1,16 @@
 package models
 
 import io.github.nthportal.paste.core.IdManager.{readIdLength, revisionIdLength}
+import io.github.nthportal.paste.core.conf.Conf.Static._
 import slick.driver.SQLiteDriver.api._
 import slick.lifted.Tag
 
 class PasteData(tag: Tag) extends Table[PasteDatum](tag, "READ_DATA") {
   def readId = column[String]("READ_ID", O.PrimaryKey, O.Length(readIdLength, varying = false))
   def fileRevision = column[String]("FILE_REVISION_ID", O.Length(revisionIdLength, varying = false))
-  def title = column[Option[String]]("TITLE", O.Default(None))
-  def author = column[Option[String]]("AUTHOR", O.Default(None))
-  def description = column[Option[String]]("DESCRIPTION", O.Default(None))
+  def title = column[Option[String]]("TITLE", O.Default(None), O.Length(maxTitleLengthChars, varying = true))
+  def author = column[Option[String]]("AUTHOR", O.Default(None), O.Length(maxAuthorLengthChars, varying = true))
+  def description = column[Option[String]]("DESCRIPTION", O.Default(None), O.Length(maxDescriptionLengthChars, varying = true))
   def unixExpiration = column[Option[Long]]("EXPIRATION", O.Default(None))
   def editable = column[Boolean]("EDITABLE", O.Default(true))
   def deletable = column[Boolean]("DELETABLE", O.Default(true))
